5cfa8ec2b28f77a985e56dee74703fbd81b24fdd
Moa Qvarnlöf <moaqva@rackham1.uppmax.uu.se>
2023-04-13 18:56:27 +0200

    job quast

---
+++ code/2_assembly_evaluation/quast_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2023-2-8
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 1:00:00
+#SBATCH -J 02_01_assembly_evaluation_quast
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user moa.qvarnlof.4873@student.uu.se
+
+# Load modules
+module load bioinfo-tools quast
+
+
+output_directory="/home/moaqva/GENOMEANALYSIS/Genome-Analysis-Project/analyses/02_genome_assembly/02_01_assembly_evaluation"
+
+file="/home/moaqva/GENOMEANALYSIS/Genome-Analysis-Project/analyses/02_genome_assembly/01_01_assembly_canu.contigs.fasta"
+
+quast.py -o $output_directory $file
