3bfb0e5a337e01fe59ef76bae27c9743ba09fa42
Moa Qvarnlöf <moaqva@rackham1.uppmax.uu.se>
2023-04-13 19:24:44 +0200

    job prokka

---
+++ code/3_structural_annotation/prokka_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2023-2-8
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 1:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user moa.qvarnlof.4873@student.uu.se
+
+# Load modules
+module load bioinfo-tools prokka
+
+output_directory="/home/moaqva/Genome-Analysis-Project/analyses/03_structural_annotation/"
+file="/home/moaqva/Genome-Analysis-Project/analyses/01_genome_assembly/01_01_assembly_canu.contigs.fasta"
+
+prokka --outdir $output_directory --force $file
